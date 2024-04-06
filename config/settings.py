04adeba5212c56df7fc23a72a6bf7ecce73316c6
shahdin <shahdin.shahdaei@gmail.com>
2024-04-06 15:15:45 +0330

    complete docker files configuration besides adding dockerignore

--- config/settings.py
+++ config/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'postgres2',
+       'USER': 'postgres2',
+       'PASSWORD': 'mobina45',
+       'HOST': 'db',
+       'PORT': 5432,
+   }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
