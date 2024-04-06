b0e3ec6d30fc1a5408de071690b6e4ec53b15a33
shahdin <shahdin.shahdaei@gmail.com>
2024-04-06 17:09:38 +0330

    create account app and custom user and applied all migrations to DB

---
+++ accounts/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from.forms import CustomUserChangeForm, CustomUserCreationForm 
+
+# models admin
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "first_name", "last_name", "email",)
+            },
+        ),
+    )
+
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'get_group']
+
+    @admin.display(description='groups')
+    def get_group(self, obj):
+        return ','.join([group.name for group in obj.groups.all()])
+
+
+# # registering models
+# admin.site.register(CustomUser, UserAdmin)
+
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
