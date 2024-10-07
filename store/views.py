96302646370f5b15a31aeb2bb3c36b5dd407581c
shahdin_sh <shahdin@Shahdins-MacBook-Pro.local>
2024-10-06 21:07:22 -0700

    change payment parameters for Zarinpal Gateway upgrade

--- store/views.py
+++ store/views.py
@@ -320,7 +320,7 @@ class PaymentProcess(APIView):
 
         if payment_request_status == 'OK':
             # payment verification request to zarinpal 
-            verify_request_url = "https://sandbox.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
+            verify_request_url = "https://sandbox.zarinpal.com/pg/v4/payment/verify.json"
            
             request_header = {
                 'accept': 'application/json',
@@ -330,9 +330,9 @@ class PaymentProcess(APIView):
             payment_data = request.session['payment_data']
 
             data_body = {
-                'MerchantID': payment_data['merchant_id'],
-                'Amount': payment_data['amount'],
-                'Authority': authority
+                'merchant_id': payment_data['merchant_id'],
+                'amount': payment_data['amount'],
+                'authority': authority
             }
 
             response = requests.post(url=verify_request_url, data=json.dumps(data_body), headers=request_header)
@@ -343,20 +343,20 @@ class PaymentProcess(APIView):
                 print(response.json())
                 return Response({"error": "Invalid JSON response from the payment gateway."}, status=status.HTTP_400_BAD_REQUEST)
             
-            if data['Status'] == 100:
+            if data['data']['code'] == 100:
 
                 # set the order status to paid and save it in DB
                 order_obj = Order.objects.select_related('customer').prefetch_related('items').get(id=payment_data['order_id'])
                 order_obj.status = 'paid'
                 order_obj.save()
 
-                return Response(f"Transaction success. | RefID: {data['RefID']}.", status=status.HTTP_200_OK)
+                return Response(f"Transaction success. | ref_id: {data['data']['ref_id']}.", status=status.HTTP_200_OK)
             
-            elif data['Status'] == 101:
-                return Response(f"Transaction is submitted before. | RefID: {data['RefID']}", status=status.HTTP_200_OK)
+            elif data['data']['code'] == 101:
+                return Response(f"Transaction is submitted before. | ref_id: {data['data']['ref_id']}", status=status.HTTP_200_OK)
             
             else:
-                return Response(f"Transaction failed. | Status: {data['Status']}", status=status.HTTP_400_BAD_REQUEST)
+                return Response(f"Transaction failed. | Status: {data['code']}", status=status.HTTP_400_BAD_REQUEST)
                         
         elif payment_request_status == 'NOK':
             return Response('Transaction failed or canceled by user.', status=status.HTTP_400_BAD_REQUEST)
@@ -371,7 +371,7 @@ class PaymentProcess(APIView):
         order_obj = Order.objects.select_related('customer').prefetch_related('items').get(id=order_id)
         
         # payment data request to zarinpal
-        zarinpal_request_url = 'https://sandbox.zarinpal.com/pg/rest/WebGate/PaymentRequest.json'
+        zarinpal_request_url = 'https://sandbox.zarinpal.com/pg/v4/payment/request.json'
 
         request_header = {
             'accept': 'application/json',
@@ -379,10 +379,10 @@ class PaymentProcess(APIView):
         }
 
         data_body = {
-            'MerchantID': '1344b5d5-0048-11e8-94db-005056a205be',
-            'Amount': order_obj.total_items_price,
-            'Description': f'Transaction for {order_obj.customer} customer | OrderID: {order_obj.id}',
-            'CallbackURL': 'http://127.0.0.1:8000/store/payment'
+            'merchant_id': '1344b5d5-0048-11e8-94db-005056a205be',
+            'amount': order_obj.total_items_price,
+            'description': f'Transaction for {order_obj.customer} customer | OrderID: {order_obj.id}',
+            'callback_url': 'http://0.0.0.0:8000/store/payment'
         }
 
         response = requests.post(url=zarinpal_request_url, data=json.dumps(data_body), headers=request_header)
@@ -391,25 +391,25 @@ class PaymentProcess(APIView):
             data = response.json()
         except json.JSONDecodeError as e:
             return Response({"error": "Invalid JSON response from the payment gateway."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        authority = data['data']['authority']
 
-        authority = data['Authority']
-    
-        if data['Status'] == 100 and 'errors' not in data and authority != '':
+        if data['data']['code'] == 100 and data['data']['message'] == 'Success' and data['errors'] == []:
             # delete the session about previous transaction
             if request.session.get('payment_data'):
                 del request.session['payment_data']
 
             # if post request to zarinpal was successful store payment data in session in order to have them in GET request
             request.session['payment_data'] = {
-                'merchant_id' : data_body['MerchantID'],
-                'amount': data_body['Amount'],
+                'merchant_id' : data_body['merchant_id'],
+                'amount': data_body['amount'],
                 'order_id': order_id
             }
 
             payment_url = 'https://sandbox.zarinpal.com/pg/StartPay/{authority}'.format(authority=authority)
             return Response(f'paymant_url: {payment_url}', status=status.HTTP_200_OK)
         else:
-            return Response(f"failed because of errors : {data.get('errors')}", status=status.HTTP_400_BAD_REQUEST)
+            return Response(f"request failed, errors : {data.get('errors')}", status=status.HTTP_400_BAD_REQUEST)
     
     def get_throttles(self):
         self.throttle_scope = 'payment'
