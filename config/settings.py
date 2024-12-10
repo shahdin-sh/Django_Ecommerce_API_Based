2fe4e876d550d49ada1bfb52b079ec089c5f47ed
shahdin <shahdin.shahdaei@gmail.com>
2024-12-10 15:29:55 -0800

    Add ALLOWED_HOSTS to .env

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
